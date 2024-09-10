d3fbec21ac8ae97308fcf65223ddbf8a1ebbc02c
Carlos Roberto <crrsj1@gmail.com>
2024-09-10 19:05:46 -0300

    Criando a classe de servico: TransacaoServico

--- src/main/java/br/com/credito/repositorio/TransacaoRepositorio.java
+++ src/main/java/br/com/credito/repositorio/TransacaoRepositorio.java
@@ -1,5 +1,11 @@
 package br.com.credito.repositorio;
 
-public interface TransacaoRepositorio {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.credito.dominio.Transacao;
+
+public interface TransacaoRepositorio extends JpaRepository<Transacao, UUID>{
 
 }
---
+++ src/main/java/br/com/credito/servico/TransacaoServico.java
@@ -0,0 +1,9 @@
+package br.com.credito.servico;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransacaoServico {
+
+	
+}
