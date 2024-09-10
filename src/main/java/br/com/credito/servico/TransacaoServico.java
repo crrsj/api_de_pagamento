a242bad56d13c2a0a688b960ae2a41dd3054195b
Carlos Roberto <crrsj1@gmail.com>
2024-09-10 19:55:54 -0300

    Criando o metodo de atualizar e excluir na classe de serviço

--- src/main/java/br/com/credito/servico/TransacaoServico.java
+++ src/main/java/br/com/credito/servico/TransacaoServico.java
@@ -1,6 +1,8 @@
 package br.com.credito.servico;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,20 @@ public class TransacaoServico {
 	public List<TransacaoDto>listarTodas(){
 		var lista = transacaoRepositorio.findAll();
 		return lista.stream().map(TransacaoDto::new).toList();
+	}
+	
+	public Transacao buscarPorId(UUID id) {
+		Optional<Transacao>buscar = transacaoRepositorio.findById(id);
+		return buscar.get();
+	}
+	
+	public Transacao atualizarTransacao(TransacaoDto transacaoDto,UUID id) {
+		var atualizar = new Transacao(transacaoDto);
+		atualizar.setId(id);
+		return transacaoRepositorio.save(atualizar);
+	}
+	
+	public void excluirTransacao(UUID id) {
+		transacaoRepositorio.deleteById(id);
 	}
 }
